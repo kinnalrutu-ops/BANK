e658ad9a4b9ca545c0f7fe43d8ff68d41b0af599
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 14:33:42 +0530

    save

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,17 @@
 
-
 initial_balance = float(input("Enter Initial Balance (₹): "))
 deposit = float(input("Enter Deposit Amount (₹): "))
 
 new_balance = initial_balance + deposit
-
 print("Initial Balance: ₹", initial_balance)
 print("Deposit: ₹", deposit)
 print("New Balance after deposit: ₹", new_balance)
+
+withdraw = float(input("Enter Withdraw Amount (₹): "))
+
+if withdraw > new_balance:
+    print("Insufficient Balance!")
+else:
+    final_balance = new_balance - withdraw
+    print("Withdraw: ₹", withdraw)
+    print("Final Balance: ₹", final_balance)
